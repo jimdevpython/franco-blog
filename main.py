50630d3c1e06fbbc212458cff39f7bae09371d56
Jim Monforte <jimmonforte@gmail.com>
2022-01-24 20:26:26 +0800

    Add environment variables, change DB from SQLITE to PosgreSQL

--- main.py
+++ main.py
@@ -23,13 +23,17 @@ class MySQLAlchemy(SQLAlchemy):
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "postgres://nbzwmumirqfppi:47b788a8881e38177f5f4a4a667befc7cd0271f42c46d88081e7a0e2adb49ca6@ec2-23-20-73-25"
+    ".compute-1.amazonaws.com:5432/dfhcarujoqmu5i "
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = MySQLAlchemy(app)
 
